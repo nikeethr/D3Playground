87d1abc88a0b1d96a9f41a5e2ae0789f1a9045a1
Nikeeth Ramanathan <Nikeeth.Ramanathan@bom.gov.au>
2018-02-15 16:30:39 +1100

    - pi chart with duration

--- maps/d3-pi.js
+++ maps/d3-pi.js
@@ -32,13 +32,14 @@ function DrawInnerArc(d, svg) {
 		.attr("transform",
 			  "translate(" + width/2 + "," + height/2 + ")");
 
-	var innerArc = gPie.selectAll("arc")
+	var innerArc = gPie.selectAll("innerArc")
 		.data(pie(d))
 		.enter()
 		.append("g")
 		.attr("class", "arc")
 
 	var sumViews = d3.sum(d.map(x => x['ScreenViews']));
+	var maxDuration = 300;
 
 	function handleInnerArcMouseover(d, i) {
 		elem = d3.select(this)
@@ -58,6 +59,9 @@ function DrawInnerArc(d, svg) {
 		d3.select("#percentage").text(percentageString);
 		d3.select("#numusers").text(usersString);
 		d3.select("#explanation").style("visibility", "");
+		d3.select("#duration").style("visibility", "");
+
+		DrawOuterArc(d, gPie, maxDuration);
 	}
 
 	function handleInnerArcMouseout(d, i) {
@@ -65,6 +69,9 @@ function DrawInnerArc(d, svg) {
 		innerArc.selectAll("path")
 			.style("opacity", 1.)
 		d3.select("#explanation").style("visibility", "hidden");
+		d3.select("#duration").style("visibility", "hidden");
+
+		RemoveOuterArc(gPie);
 	}
 	
 	innerArc.append("path")
@@ -74,8 +81,43 @@ function DrawInnerArc(d, svg) {
 		.on("mouseout", handleInnerArcMouseout);
 }
 
+function RemoveOuterArc(svg) {
+	svg.selectAll("#outerArc").remove();
+}
 
-function DrawOuterArc(d, svg) {
+function DrawOuterArc(d, svg, tD) {
+	var radius = 140;
+	var thickness = 16;
+
+	var data = [d.data.ScreenDuration, tD - d.data.ScreenDuration];
+
+	var path = d3.arc()
+		.innerRadius(radius - thickness/2)
+		.outerRadius(radius + thickness/2);
+
+	var pie = d3.pie()
+		.sort(null)
+		.value(function(d) { return d3.max([d3.min([d, tD]), 0]); });
+
+	var outerArc = svg.selectAll("outerArc")
+		.data(pie(data))
+		.enter()
+		.append("g")
+		.attr("class", "arc")
+		.attr("id", "outerArc");
+
+	var outerArcCol = ["#fe9929", "#bababa"] ;
+
+	outerArc.append("path")
+		.attr("d", path)
+		.attr("fill", function(d, i) { return outerArcCol[i % 2]; });
+
+	var durationStr = 
+		d.data.ScreenDuration > tD
+		? '>' + tD + ' min'
+		: d.data.ScreenDuration + ' min';
+
+	d3.select("#duration").text(durationStr);
 }
 
 d3.csv("screen.csv",
